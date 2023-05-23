9c0a70ca5bc552f48d2ddfac5d4b05786636f316
Joacil <joacil.amarante@gmail.com>
2023-05-23 18:20:33 -0400

    .

---
+++ 11-Visibilidade/Conta.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_Visibilidade
+{
+    public class Conta
+    {
+        public string agencia;
+        private double saldo;
+        public Cliente cliente;
+
+
+        public void DepositarSaldo(double saldoParm)
+        {
+            if (saldoParm > 0)
+            {
+                this.saldo = saldoParm;
+            }
+            else
+            {
+                Console.WriteLine("Saldo inválido!");
+            }
+        }
+
+
+        public void retornaSaldo(Cliente cliente)
+        {
+            Console.WriteLine(this.saldo);
+        }
+
+    }
+}
---
+++ 11-Visibilidade/Program.cs
@@ -0,0 +1,44 @@
+﻿
+
+
+
+using _11_Visibilidade;
+
+Cliente cliente1 = new Cliente();
+cliente1.nome = "Davi";
+
+
+Cliente cliente2 = new Cliente();
+cliente2.nome = "Maria";
+
+
+
+Conta conta1 = new Conta();
+conta1.agencia = "11111";
+conta1.DepositarSaldo(100);
+conta1.cliente = cliente1;
+
+
+Conta conta2 = new Conta();
+conta2.agencia = "22222";
+conta2.DepositarSaldo(150);
+conta2.cliente = cliente2;
+
+
+
+
+
+
+
+Console.WriteLine("---------------------- CLIENTE 1 ---------------------");
+Console.WriteLine("Agencia:  " + conta1.agencia);
+Console.WriteLine("Nome:  " + conta1.cliente.nome);
+conta1.retornaSaldo(cliente1);
+
+
+Console.WriteLine("---------------------- CLIENTE 2 ---------------------");
+Console.WriteLine("Agencia:  " + conta2.agencia);
+Console.WriteLine("Nome:  " + conta2.cliente.nome);
+conta2.retornaSaldo(cliente2);
+
+//Console.WriteLine("Cliente:  " + conta1.retornaSaldo);
