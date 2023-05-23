27c6f196ac9bc75127fc0bbe2bcf4538501316fa
Joacil <joacil.amarante@gmail.com>
2023-05-23 11:56:05 -0400

    23-05-2023

---
+++ 9-Classes/ContaCorrente.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_Classes
+{
+    public class ContaCorrente
+    {
+        public int numero_agencia;
+        public string conta;
+        public string titular;
+        public double saldo;
+
+
+        public void Depositar(double valor)
+        {
+            this.saldo += valor;
+        }
+
+        public void Saque(double Valor)
+        {
+            if (this.saldo < Valor)
+            {
+                Console.WriteLine("Operação cancelada, você não possui saldo insuficiente. Seu limite máximo para saque hoje é de: R$" + this.saldo);
+            }
+            else
+            {
+                this.saldo -= Valor;
+            }
+
+        }
+
+        public bool Transferir(double Valor, ContaCorrente Destino)
+        {
+            if (this.saldo < Valor)
+            {
+                this.saldo += Valor;
+                Console.WriteLine("Saldo ínsuficiente para TRANSFERENCIA!!!");
+                return false;
+            }
+            else
+            {
+                this.Saque(Valor);
+                Destino.Depositar(Valor);
+                return true;
+            }
+
+        }
+
+    }
+
+
+
+}
---
+++ 9-Classes/Program.cs
@@ -0,0 +1,35 @@
+﻿using _9_Classes;
+
+
+// tipo / var / instancia
+
+ContaCorrente contaDoJoacil = new ContaCorrente();
+contaDoJoacil.numero_agencia = 2373;
+contaDoJoacil.titular = "Joacil Amarante";
+contaDoJoacil.conta = "60509-3";
+contaDoJoacil.saldo = 100;
+
+
+Console.WriteLine("Saldo Inicial: " + contaDoJoacil.saldo);
+contaDoJoacil.Depositar(150);
+Console.WriteLine("Após o deposito seu Novo Saldo: " + contaDoJoacil.saldo);
+contaDoJoacil.Saque(100);
+Console.WriteLine("Saldo após saque: " + contaDoJoacil.saldo);
+
+
+
+
+ContaCorrente ContaDaVitoria = new ContaCorrente();
+ContaDaVitoria.numero_agencia = 111;
+ContaDaVitoria.titular = "Vitoria de Paula";
+ContaDaVitoria.conta = "222";
+ContaDaVitoria.saldo = 0;
+Console.WriteLine("--------------------------------------------");
+Console.WriteLine("Saldo Vitoria ANTES TRANSFERENCIA: " + ContaDaVitoria.saldo);
+contaDoJoacil.Transferir(50, ContaDaVitoria);
+Console.WriteLine("Saldo Vitoria: " + ContaDaVitoria.saldo);
+Console.WriteLine("Saldo JOACIL: " + contaDoJoacil.saldo);
+
+
+
+
