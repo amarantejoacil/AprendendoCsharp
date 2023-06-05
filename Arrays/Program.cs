d8ea3fc5a01bb879748af5ebfe379c6f853f76f1
Joacil <joacil.amarante@gmail.com>
2023-06-05 18:47:51 -0400

    05-06-2023

---
+++ Arrays/Program.cs
@@ -0,0 +1,83 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+
+        string aulaIntro = "Introdução a coleções";
+        string aulaMod = "Modelando a Classe";
+        string aulaSet = "Trabalhando com Set";
+
+        //1 forma: forma dinamica
+        string[] aulas = new string[]
+        {
+            aulaIntro, aulaMod, aulaSet
+        };
+
+        //2 forma: forma manual
+        //string[] aulas = new string[3];
+        //aulas[0] = aulaIntro;
+        //aulas[1] = aulaMod;
+        //aulas[2] = aulaSet;
+
+
+        Console.WriteLine("-------------------------Imprimindo Array-------------------------");
+        foreach (string s in aulas)
+        {
+            Console.WriteLine(s);
+        }
+
+        Console.WriteLine("-------------------------Usando o Reverse-------------------------");
+        Array.Reverse(aulas);
+        foreach (string s in aulas)
+        {
+            Console.WriteLine(s);
+        }
+
+
+        Console.WriteLine("-------------------------Redimensionando o Array para 2 -------------------------");
+        Array.Resize(ref aulas, 2);
+        foreach (string s in aulas)
+        {
+            Console.WriteLine(s);
+        }
+
+
+        Console.WriteLine("-------------------------Ordernar o Array -------------------------");
+
+        Array.Resize(ref aulas, 3);//retornando para 3 elementos
+
+        Array.Sort(aulas);
+        foreach (string s in aulas)
+        {
+            if (s == null)
+            {
+                Console.WriteLine("valor nulo");
+            }
+            else
+            {
+                Console.WriteLine(s);
+            }
+
+        }
+
+        Console.WriteLine("-------------------------Copiando o Array -------------------------");
+
+        string[] copiaDoarray = new string[3];
+        Array.Copy(aulas, 0, copiaDoarray, 0, aulas.Length);
+        foreach (string sCopia in copiaDoarray)
+        {
+            Console.WriteLine(sCopia);
+        }
+
+        Console.WriteLine("-------------------------Clone o Array -------------------------");
+        string[] clone = aulas.Clone() as string[];
+        foreach (string sClone in copiaDoarray)
+        {
+            Console.WriteLine(sClone);
+        }
+
+        Console.WriteLine("-------------------------Limpando Clone o Array -------------------------");
+        Array.Clear(clone, 1, 2);
+
+    }
+}
---
+++ WorkWithLista/Program.cs
@@ -0,0 +1,52 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+
+        string primeiroProduto = "Primeiro Produto";
+        string segundoProduto = "Segundo Produto";
+        string terceiroProduto = "Terceiro Produto";
+
+        List<string> produtos = new List<string>() { primeiroProduto, segundoProduto, terceiroProduto };
+
+        //para cada produto, execute o cod. dentro das chaves ou para atender faça determina expressão
+        produtos.ForEach(produto => { Console.WriteLine(produto); });
+
+
+        produtos[0] = "Novo primeiro produto";
+        Console.WriteLine("----------------------------Imprimindo após alterar o primeiro index-----------------------------");
+        produtos.ForEach(produto => { Console.WriteLine(produto); });
+
+
+        Console.WriteLine("----------------------------Removendo a teceria posicao-----------------------------");
+
+        produtos.RemoveAt(produtos.Count - 1);
+        produtos.ForEach(produto => { Console.WriteLine(produto); });
+
+
+
+        Console.WriteLine("----------------------------Adicionando novo produto -----------------------------");
+        produtos.Add("Novo produto adicionado pelo Add");
+        produtos.ForEach(produto => { Console.WriteLine(produto); });
+
+
+
+        Console.WriteLine("count: " + produtos.Count);
+        Console.WriteLine("----------------------------Copia da lista  produto -----------------------------");
+        List<string> copiaListaDeProduto = produtos.GetRange(0, 1);
+        copiaListaDeProduto.ForEach(novoProduto => { Console.WriteLine(novoProduto); });
+
+
+        Console.WriteLine("----------------------------Clonar da lista  produto -----------------------------");
+        List<string> Clone = new List<string>(produtos);
+        foreach (var c in Clone)
+        {
+            Console.WriteLine(c);
+        }
+
+        Console.WriteLine("----------------------------Removendo com range-----------------------------");
+        Clone.RemoveRange(0, 2);
+        Clone.ForEach(c => Console.WriteLine(c));
+
+    }
+}
